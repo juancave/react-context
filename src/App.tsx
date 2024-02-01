c918a1756b37f08e23a053c3b8eb78cb15ab03bd
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-02-01 11:30:10 -0500

    Implementing image size context in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,77 @@
-import React from 'react';
+import { useContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { places, Place as PlaceInterface } from './util/data';
+import { getImageUrl } from './util/util';
+import { ImageSizeContext } from './context';
+
+interface Props {
+  readonly place: PlaceInterface;
+}
+
+function List() {
+  const listItems = places.map(place =>
+    <div className='List-Item' key={place.id}>
+      <Place place={place} />
+    </div>
+  );
+
+  return (
+    <div className='List-Container'>
+      {listItems}
+    </div>
+  );
+}
+
+function Place({ place }: Props) {
+  return (
+    <>
+      <PlaceImage place={place} />
+      <p>
+        <b>{place.name}</b>
+        {': ' + place.description}
+      </p>
+    </>
+  );
+}
+
+function PlaceImage({ place }: Props) {
+  const imageSize = useContext(ImageSizeContext);
+
+  return (
+    <img
+      className='Place-Image'
+      src={getImageUrl(place.imageId)}
+      alt={place.name}
+      width={imageSize}
+      height={imageSize}
+    />
+  );
+}
+
+function ContextContainer() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+
+  return (
+    <div className='Context-Container'>
+      <label>
+        <input
+          type="checkbox"
+          checked={isLarge}
+          onChange={e => {
+            setIsLarge(e.target.checked);
+          }}
+        />
+        Use large images
+      </label>
+      <hr />
+      <ImageSizeContext.Provider value={imageSize}>
+        <List />
+      </ImageSizeContext.Provider>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -8,17 +79,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          React Context
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <ContextContainer />
     </div>
   );
 }
